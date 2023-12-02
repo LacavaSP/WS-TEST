75cc673e1f69317397af988755896e68a52b71aa
Eduardo Lima Lacava <86937690+LacavaSP@users.noreply.github.com>
2023-12-02 12:01:09 -0300

    WS SERVER

---
+++ backend.js
@@ -0,0 +1,53 @@
+const WebSocket = require('ws')
+const pessoasConectadas = []
+const webSocketServidor = new WebSocket.Server({ port: 3000 })
+
+const express = require('express')
+const servidor = express()
+const porta = 5000
+servidor.use(express.json())
+servidor.use(express.static('public'))
+
+const notificacao = (mensagem) => ({
+    mensagem: mensagem,
+    quemEnviou: 'Eduardo Lacava',
+    dataEnvio: new Date()
+  })
+
+servidor.listen(5000, '195.35.37.40',() => {
+    console.log(`SERVIDOR INICIADO EM HTTP://LOCALHOST:${porta}`)
+})
+
+servidor.get('/api/frutas', async (req, res) => {
+    return res.status(200).json(['banana', 'uva', 'melao', 'abacaxi', 'pitaia'])
+})
+
+
+webSocketServidor.on('connection', (socket) => {
+    console.log('Cliente conectado.');
+    pessoasConectadas.push(socket)
+    // Enviar uma mensagem para o cliente quando ele se conectar
+    socket.send(JSON.stringify(notificacao('Olá, sou o WebSocket Techers :)!')));
+
+    // Lidar com mensagens do cliente
+    socket.on('message', (message) => {
+        console.log(`Mensagem recebida: ${message}`);
+
+        // Enviar uma resposta de volta para o cliente
+        socket.send(`Você disse: ${message}`);
+    });
+
+    // Lidar com o evento de fechamento da conexão
+    socket.on('close', () => {
+        console.log('Cliente desconectado.');
+    });
+});
+
+servidor.post('/api/envia-mensagem-ws', async (req, res) => {
+    pessoasConectadas.forEach((pessoa) => pessoa.send(JSON.stringify(notificacao(req.body.mensagem))))
+    return res.status(200).json()
+})
+
+/*setInterval(() => {
+    pessoasConectadas.forEach((pessoa) => pessoa.send(JSON.stringify(notificacao("MENSAGEM"))))
+}, 1500)*/
